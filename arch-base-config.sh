3650fa7c4009cf3fa5fb29c22c1380bc7f9fe2ee
Syg <872633+SYGINT@users.noreply.github.com>
2021-10-25 03:42:53 -0700

    Update arch-base-config.sh

--- arch-base-config.sh
+++ arch-base-config.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
+# locale and time
 ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 hwclock --systohc
 sed -i '177s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=us" >> /etc/vconsole.conf
+
+# hostname and hosts
 echo "satan" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 satan.localdomain satan" >> /etc/hosts
-echo root:password | chpasswd
 
-# You can add xorg to the installation packages, I usually add it at the DE or WM install script
-# You can remove the tlp package if you are installing on a desktop or vm
 
 pacman -S grub grub-btrfs networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers xdg-user-dirs xdg-utils bluez bluez-utils cups alsa-utils pulseaudio rsync reflector acpi acpi_call tlp os-prober ntfs-3g terminus-font
 
-pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+pacman -S nvidia nvidia-utils nvidia-settings
 
+# grub
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
+# services
 systemctl enable NetworkManager
 systemctl enable bluetooth
 systemctl enable cups.service
@@ -30,9 +32,14 @@ systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable acpid
 
+# root
+echo root:password | chpasswd
+
+# create user
 useradd -m syg
 echo syg:password | chpasswd
-
 echo "syg ALL=(ALL) ALL" >> /etc/sudoers.d/syg
+
+
 
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
